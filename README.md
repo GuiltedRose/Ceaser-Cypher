f9ceba3b20ad9d0ec98629dd2efd995b749d2c22
GuiltedRose <alisonroseclark001@gmail.com>
2022-05-11 01:53:47 -0400

    Finished result

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Ceaser-Cypher
+
+I learned that if you push the string past 26; it gets a little wonky. I am working on changing the code base slightly, but I can't figure out how to shorten it.
--- cypher.rb
+++ cypher.rb
@@ -1,3 +1,28 @@
 # This is a cypher project.
 
-puts ('hello world')
+# defining our method:
+
+def ceaser_cypher(text, shift)
+
+    # loops through the text to encrypt the message
+   for i in 0...text.length do
+        char_code = text[i].ord
+
+        (a, z) = case char_code
+            when 97..122 then [97, 122]   # a-z
+            when 65..90  then [65, 90]    # A-Z
+            else next
+        end
+        rotate = shift > 0 ? 26 : -26 # rotates the array we created with  char_code.
+  
+      char_code += shift
+      char_code -= rotate unless char_code.between?(a, z) # these lines shift the code unless it falls between a & z.
+  
+      text[i] = char_code.chr #puts it back together; can also do text[i].join.
+   end
+end
+
+message = "You cannot read ME! <3" 
+ceaser_cypher(message, 37) # shows what happens when you exceed 26.
+
+puts(message) #prints the encrypted output.
